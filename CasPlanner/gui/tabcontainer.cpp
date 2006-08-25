8a54b2925478b63ca1420e918e125c1008c9d5a8
Tarek <tarek@tarektaha.com>
2006-08-25 05:06:00 +0000

    - Map Features added

--- CasPlanner/gui/tabcontainer.cpp
+++ CasPlanner/gui/tabcontainer.cpp
@@ -4,8 +4,8 @@ TabContainer::TabContainer(QWidget *parent,RobotManager *rob)
     : QTabWidget(parent),
       robotManager(rob)
 {
-    navCon = new NavContainer(parent,rob);
-    mapViewer = new MapViewer(parent);
+    navCon = 	new NavContainer(parent,rob);
+    mapViewer = new MapViewer(parent,rob);
     //sensorsGui = new SensorsGui(parent,rob);
     addTab(navCon, "Navigation Panel"); 
     addTab(mapViewer, "OpenGl Map Viewer"); 
