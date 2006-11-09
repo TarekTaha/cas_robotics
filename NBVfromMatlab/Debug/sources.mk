ed78f27b0e6ba54ef02f3fbc3e4de6b88701c5a1
gpaul1611 <gpaul1611@dfc047db-ddda-4613-bb57-a2f62a239487>
2006-11-09 06:08:46 +0000

    initial import

---
+++ NBVfromMatlab/Debug/sources.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+CPP_SRCS := 
+C_UPPER_SRCS := 
+S_SRCS := 
+CC_SRCS := 
+ASM_SRCS := 
+CXX_SRCS := 
+OBJ_SRCS := 
+O_SRCS := 
+DEPS := 
+OBJS := 
+EXECUTABLES := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+
