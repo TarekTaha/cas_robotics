d85af2477d8fd569e48a57c3d60bd86512aae878
tarek_taha <tarek_taha@dfc047db-ddda-4613-bb57-a2f62a239487>
2006-12-08 13:36:26 +0000

    - OOP restructure - Still some outstanding Issues

--- PointCloudPlotter/LaserBuffer.cpp
+++ PointCloudPlotter/LaserBuffer.cpp
@@ -10,5 +10,27 @@ LaserBuffer::~LaserBuffer()
 
 LaserBuffer::LaserBuffer(int servoPoints, int laserPoints)
 {
-	
+	this->numScans = servoPoints;
+	this->pointsPerScan = laserPoints;
+	scans.assign(servoPoints,laserPoints);
+	triangles.reserve(400000);
+}
+
+void LaserBuffer::clear()
+{
+	for(int i=0;i<scans.size();i++)
+		for(int j=0;j<scans[i].points.size();j++)
+		{
+			scans[i].points[j].setXYZ(0,0,0);
+		}
+		numScans=0;
+		pointsPerScan=0;
+		numtri =0;
+		lastScanCount=0;		
+}
+
+LaserBuffer::~LaserBuffer()
+{
+	triangles.clear();
+	scans.clear();
 }
