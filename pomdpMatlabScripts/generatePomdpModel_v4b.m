7b37492d19a6e7277562d7b30a817e49a9e46810
tarek_taha <tarek_taha@dfc047db-ddda-4613-bb57-a2f62a239487>
2007-05-21 05:25:24 +0000

    Added observation Modelling

--- pomdpMatlabScripts/generatePomdpModel_v4b.m
+++ pomdpMatlabScripts/generatePomdpModel_v4b.m
@@ -3,42 +3,46 @@ function generatePomdpModel(modelFileName,observationsDataFile)
 % syntaxt. You have to specify the actions, observations and the
 % uncertainty in their readings.
 
+pomdpModel.outputFile  = modelFileName;
+pomdpModel.obsDataFile = observationsDataFile;
+
 fid = fopen(modelFileName, 'wb');
 global mapTopology;
 discount = 0.95;
-actions = {'North','South','East','West','Stop'};
-observations = {'Up','Down','Right','Left','Nothing'};
+pomdpModel.actions = {'North','South','East','West','Stop'};
+pomdpModel.observations = {'Up','Down','Right','Left','Nothing'};
+
 % Percentage Uncertainty in the actions
-%actionsUncertainty = [10,10,20,20,10];    
-actionsUncertainty = [0,0,0,0,0];
+%pomdpModel.actionsUncertainty = [10,10,20,20,10];    
+pomdpModel.actionsUncertainty = [0,0,0,0,0];
 % Percentage Uncertainty in the Observations
-%bservationsUncertainty = [10,10,10,10,15];
-observationsUncertainty = [0,0,0,0,0];
+%pomdpModel.observationsUncertainty = [10,10,10,10,15];
+pomdpModel.observationsUncertainty = [0,0,0,0,0];
 
 % Construct the Graph network by defining the number of states and the
 % translation inbetween the states. This will be used to build the
 % translational probobilities with added uncertainties.
 
-% define the destinations 
-destinations = {'s3d1','s7d2'};
-numSpacialStates = 9;
+% define the destinations
+pomdpModel.destinations = {'s3d1','s7d2'};
+pomdpModel.numSpatialStates = 9;
 % Here we are building a model that is a cross product between states
 % representiong spaces in the environment and destinations that the user
 % wants to go to.
 
-mapTopology.nnodes = numSpacialStates*length(destinations);
-mapTopology.network = zeros(mapTopology.nnodes,length(actions));
+mapTopology.nnodes = pomdpModel.numSpatialStates*length(pomdpModel.destinations);
+mapTopology.network = zeros(mapTopology.nnodes,length(pomdpModel.actions));
     
-for i=0:(length(destinations)-1)
-    mapTopology.network(1+i*numSpacialStates,:) = [0,4,0,0,1]; 
-    mapTopology.network(2+i*numSpacialStates,:) = [0,5,0,0,2]; 
-    mapTopology.network(3+i*numSpacialStates,:) = [0,6,0,0,3]; 
-    mapTopology.network(4+i*numSpacialStates,:) = [1,7,5,0,4]; 
-    mapTopology.network(5+i*numSpacialStates,:) = [2,8,6,4,5]; 
-    mapTopology.network(6+i*numSpacialStates,:) = [3,9,0,5,6]; 
-    mapTopology.network(7+i*numSpacialStates,:) = [4,0,0,0,7]; 
-    mapTopology.network(8+i*numSpacialStates,:) = [5,0,0,0,8]; 
-    mapTopology.network(9+i*numSpacialStates,:) = [6,0,0,0,9];
+for i=0:(length(pomdpModel.destinations)-1)
+    mapTopology.network(1+i*pomdpModel.numSpatialStates,:) = [0,4,0,0,1]; 
+    mapTopology.network(2+i*pomdpModel.numSpatialStates,:) = [0,5,0,0,2]; 
+    mapTopology.network(3+i*pomdpModel.numSpatialStates,:) = [0,6,0,0,3]; 
+    mapTopology.network(4+i*pomdpModel.numSpatialStates,:) = [1,7,5,0,4]; 
+    mapTopology.network(5+i*pomdpModel.numSpatialStates,:) = [2,8,6,4,5]; 
+    mapTopology.network(6+i*pomdpModel.numSpatialStates,:) = [3,9,0,5,6]; 
+    mapTopology.network(7+i*pomdpModel.numSpatialStates,:) = [4,0,0,0,7]; 
+    mapTopology.network(8+i*pomdpModel.numSpatialStates,:) = [5,0,0,0,8]; 
+    mapTopology.network(9+i*pomdpModel.numSpatialStates,:) = [6,0,0,0,9];
 end
 
 % Write to File the top comments and warnings
@@ -50,19 +54,19 @@ fprintf(fid,'\ndiscount: %f',discount);
 fprintf(fid,'\nvalues: reward');
 
 fprintf(fid,'\n\nstates: ');
-for i=1:length(destinations)
-    for j=1:numSpacialStates
+for i=1:length(pomdpModel.destinations)
+    for j=1:pomdpModel.numSpatialStates
         fprintf(fid,'s%dd%d ',j,i);
     end
 end
 
 fprintf(fid,'\n\nactions:');
-for i=1:length(actions)
-    fprintf(fid,' %s',actions{i});
+for i=1:length(pomdpModel.actions)
+    fprintf(fid,' %s',pomdpModel.actions{i});
 end
 fprintf(fid,'\n\nobservations:');
-for i=1:length(observations)
-    fprintf(fid,' %s',observations{i});
+for i=1:length(pomdpModel.observations)
+    fprintf(fid,' %s',pomdpModel.observations{i});
 end
 startBelief = 1.0/mapTopology.nnodes;
 fprintf(fid,'\nstart:');
@@ -73,40 +77,40 @@ end
 % Build the Translation probabilities
 fprintf(fid,'\n\n# T: <action> : <start-state> : <end-state> %%f');
 fprintf(fid,'\nT: * : * : * 0.0');
-for i=1:length(actions)
+for i=1:length(pomdpModel.actions)
     % Find the number of reachable nodes (connected)
     % n = length(find(mapTopology.network(j,:)));  
-    n = length(destinations);
+    n = length(pomdpModel.destinations);
     % Uncertainty in the final state given this action is divided
     % equally to the connected neighbouring nodes
-    % actionNoise = actionsUncertainty(i)/(n-1)/100;    
-    if length(find(actionsUncertainty)) ~=0
-        actionProb = (100 - actionsUncertainty(i))/100;
-        actionNoise = actionsUncertainty(i)/(n-1)/100;
+    % actionNoise = pomdpModel.actionsUncertainty(i)/(n-1)/100;    
+    if length(find(pomdpModel.actionsUncertainty)) ~=0
+        actionProb = (100 - pomdpModel.actionsUncertainty(i))/100;
+        actionNoise = pomdpModel.actionsUncertainty(i)/(n-1)/100;
     else
         actionProb  = 1;
         actionNoise = 0;
     end
     for j=1:mapTopology.nnodes
-        if mod(j,numSpacialStates) == 0
-            state = numSpacialStates;
+        if mod(j,pomdpModel.numSpatialStates) == 0
+            state = pomdpModel.numSpatialStates;
         else
-            state = mod(j,numSpacialStates);
+            state = mod(j,pomdpModel.numSpatialStates);
         end
         
-        if floor(j/numSpacialStates) == 0 
+        if floor(j/pomdpModel.numSpatialStates) == 0 
             dest = 1;
-        elseif mod(j,numSpacialStates) == 0
-            dest  = floor(j/numSpacialStates);
+        elseif mod(j,pomdpModel.numSpatialStates) == 0
+            dest  = floor(j/pomdpModel.numSpatialStates);
         else
-            dest  = floor(j/numSpacialStates) + 1;
+            dest  = floor(j/pomdpModel.numSpatialStates) + 1;
         end
 %         % Deterministic Case
-        if length(find(actionsUncertainty)) == 0
+        if length(find(pomdpModel.actionsUncertainty)) == 0
             if (mapTopology.network(j,i)==0)
-                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',actions{i},state,dest,state,dest,actionProb);
+                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',pomdpModel.actions{i},state,dest,state,dest,actionProb);
             else
-                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',actions{i},state,dest,mapTopology.network(j,i),dest,actionProb);
+                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',pomdpModel.actions{i},state,dest,mapTopology.network(j,i),dest,actionProb);
             end
             continue;
         end
@@ -114,13 +118,13 @@ for i=1:length(actions)
         for k=1:n
             % Undefined action to this state
             if (mapTopology.network(j,i)==0)
-                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',actions{i},state,dest,state,k,1/n);
+                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',pomdpModel.actions{i},state,dest,state,k,1/n);
                 continue;
             end
             if k==dest
-                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',actions{i},state,dest,mapTopology.network(j,i),k,actionProb);
+                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',pomdpModel.actions{i},state,dest,mapTopology.network(j,i),k,actionProb);
             else
-                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',actions{i},state,dest,mapTopology.network(j,i),k,actionNoise);
+                fprintf(fid,'\nT: %s : s%dd%d : s%dd%d %f',pomdpModel.actions{i},state,dest,mapTopology.network(j,i),k,actionNoise);
             end
         end
         fprintf(fid,'\n');
@@ -133,94 +137,39 @@ end
 fprintf(fid,'\n\n# O : <action> : <end-state> : <observation> %%f');
 fprintf(fid,'\nO: * : * : * 0.0');
 
-file = textread(observationsDataFile,'%s','delimiter','\n','whitespace',' \b\t','bufsize',100000);
-k=0;
-for i=1:length(file)
-  comment=strfind(file{i},'#');
-  if ~isempty(comment)
-    file{i}(comment(1):end)=[];
-  end
-  if ~isempty(file{i})
-    k=k+1;
-    fileTemp{k}=file{i};
-  end
+
+[obsProbs obs] = learnObservationModel(pomdpModel);
+% Normalize and print to file
+for i=1:length(obs)
+    index = (obs{i}.dest-1)*pomdpModel.numSpatialStates + obs{i}.pos;
+    indx = strfind(pomdpModel.observations,obs{i}.obs);
+    for j=1:length(indx)
+        if ~isempty(indx{j})
+            obsIndx = j;
+        end 
+    end
+    obsProb = obsProbs(index,obsIndx)/sum(obsProbs(index,:));
+    fprintf(fid,'\nO: * : %s : %s %f',obs{i}.state,obs{i}.obs,obsProb);
 end
-clear file;
-
-for i=1:length(fileTemp)  
-    obs{i} = sscanf(fileTemp{i},'%d %s');
-end
-
-fprintf(fid,'\nO: * : s1d1 : Down 0.9');
-fprintf(fid,'\nO: * : s1d1 : Nothing 0.1');
-fprintf(fid,'\nO: * : s1d2 : Down 0.9');
-fprintf(fid,'\nO: * : s1d2 : Nothing 0.1');
-
-fprintf(fid,'\nO: * : s2d1 : Down 0.9');
-fprintf(fid,'\nO: * : s2d1 : Nothing 0.1');
-fprintf(fid,'\nO: * : s2d2 : Down 0.9');
-fprintf(fid,'\nO: * : s2d2 : Nothing 0.1');
-
-fprintf(fid,'\nO: * : s3d1 : Down 0.1');
-fprintf(fid,'\nO: * : s3d1 : Nothing 0.9');
-fprintf(fid,'\nO: * : s3d2 : Down 0.9');
-fprintf(fid,'\nO: * : s3d2 : Nothing 0.1');
-
-fprintf(fid,'\nO: * : s4d1 : Right 0.9');
-fprintf(fid,'\nO: * : s4d1 : Nothing 0.1');
-fprintf(fid,'\nO: * : s4d2 : Down 0.9');
-fprintf(fid,'\nO: * : s4d2 : Nothing 0.1');
-
-fprintf(fid,'\nO: * : s5d1 : Right 0.9');
-fprintf(fid,'\nO: * : s5d1 : Nothing 0.1');
-fprintf(fid,'\nO: * : s5d2 : Left 0.9');
-fprintf(fid,'\nO: * : s5d2 : Nothing 0.1');
-
-fprintf(fid,'\nO: * : s6d1 : Up 0.9');
-fprintf(fid,'\nO: * : s6d1 : Nothing 0.1');
-fprintf(fid,'\nO: * : s6d2 : Left 0.9');
-fprintf(fid,'\nO: * : s6d2 : Nothing 0.1');
-
-fprintf(fid,'\nO: * : s7d1 : Up 0.9');
-fprintf(fid,'\nO: * : s7d1 : Nothing 0.1');
-fprintf(fid,'\nO: * : s7d2 : Up 0.1');
-fprintf(fid,'\nO: * : s7d2 : Nothing 0.9');
-
-fprintf(fid,'\nO: * : s8d1 : Up 0.9');
-fprintf(fid,'\nO: * : s8d1 : Nothing 0.1');
-fprintf(fid,'\nO: * : s8d2 : Up 0.9');
-fprintf(fid,'\nO: * : s8d2 : Nothing 0.1');
-
-fprintf(fid,'\nO: * : s9d1 : Up 0.9');
-fprintf(fid,'\nO: * : s9d1 : Nothing 0.1');
-fprintf(fid,'\nO: * : s9d2 : Up 0.9');
-fprintf(fid,'\nO: * : s9d2 : Nothing 0.1');
 
 % Build Reward Function
 fprintf(fid,'\n\n# R: <action> : <start-state> : <end-state> : <observation> %%f');
 fprintf(fid,'\nR: * : * : * : * 0.0');
-for i=1:length(destinations)
-    for j=1:numSpacialStates
-        index = (i-1)*numSpacialStates + j;
-        for s=1:length(actions)
+for i=1:length(pomdpModel.destinations)
+    for j=1:pomdpModel.numSpatialStates
+        index = (i-1)*pomdpModel.numSpatialStates + j;
+        for s=1:length(pomdpModel.actions)
             if (mapTopology.network(index,s)==0) % Undefine actions are penalized
-                fprintf(fid,'\nR: %s : * : s%dd%d : * -10',actions{s},j,i);
+                fprintf(fid,'\nR: %s : * : s%dd%d : * -10',pomdpModel.actions{s},j,i);
             end
         end
     end
     fprintf(fid,'\n');
 end
-for i=1:length(destinations)
-    fprintf(fid,'\nR: Stop : %s : %s : * 10',destinations{i},destinations{i});
-    fprintf(fid,'\nR: *    : *  : %s : * 100',destinations{i});
+for i=1:length(pomdpModel.destinations)
+    fprintf(fid,'\nR: Stop : %s : %s : * 10',pomdpModel.destinations{i},pomdpModel.destinations{i});
+    fprintf(fid,'\nR: *    : *  : %s : * 100',pomdpModel.destinations{i});
 end
 clear fileTemp;
 fclose(fid);
-end
-
-function r = expandString(c,members)
-  r = strmatch(c,members,'exact');
-  if isempty(r) % apparently c is a numbered state
-    r = str2double(c)+1; % Matlab starts at 1, not 0
-  end
 end
---
+++ pomdpMatlabScripts/learnObservationModel.m
@@ -0,0 +1,73 @@
+function [obsProbs obs] = learnObservationModel(pomdpModel)
+% This function build the observation probabilities from a set of recorded
+% data. The function reads a file that contains a task on each line
+% representing a sequence of positions and observations recoreded to reach
+% a destination. 
+
+% read the file and save in buffer
+file = textread(pomdpModel.obsDataFile,'%s','delimiter','\n','whitespace',' \b\t','bufsize',100000);
+k=0;
+%ignore comments
+for i=1:length(file)
+  comment=strfind(file{i},'#');
+  if ~isempty(comment)
+    file{i}(comment(1):end)=[];
+  end
+  if ~isempty(file{i})
+    k=k+1;
+    fileTemp{k}=file{i};
+  end
+end
+clear file;
+
+% read tasks (sequence of observations and locations)
+numObs=0;
+numendLocations = 0;
+for i=1:length(fileTemp)  
+  pat='(\d)+\s*(\S+)\s*';
+  [s,f,t]=regexp(fileTemp{i},pat); 
+  last = length(t);
+  % Find the destination index from the set of given destinations
+  dest = str2num(fileTemp{i}(t{last}(1,1):t{last}(1,2)));
+  indx = strfind(pomdpModel.destinations,sprintf('s%d',dest));
+  for j=1:length(indx)
+      if ~isempty(indx{j})
+        destIndx = j;
+      end 
+  end
+  % build the observation set from this task
+  for j=1:length(t)
+      numObs = numObs + 1;
+      obs{numObs}.obs   = fileTemp{i}(t{j}(2,1):t{j}(2,2));
+      obs{numObs}.dest  = destIndx;
+      obs{numObs}.pos   = str2num(fileTemp{i}(t{j}(1,1):t{j}(1,2)));
+      obs{numObs}.state = sprintf('s%dd%d',obs{numObs}.pos,destIndx);            
+  end
+end
+
+% build the probabilities
+obsProbs = zeros(pomdpModel.numSpatialStates*length(pomdpModel.destinations),length(pomdpModel.observations));
+for i=1:length(obs)
+    index = (obs{i}.dest-1)*pomdpModel.numSpatialStates + obs{i}.pos;
+    indx = strfind(pomdpModel.observations,obs{i}.obs);
+    for j=1:length(indx)
+        if ~isempty(indx{j})
+            obsIndx = j;
+        end 
+    end    
+    obsProbs(index,obsIndx) = obsProbs(index,obsIndx) + 1;
+end
+
+% Normalize probabilities
+for i=1:length(obs)
+    index = (obs{i}.dest-1)*pomdpModel.numSpatialStates + obs{i}.pos;
+    indx = strfind(pomdpModel.observations,obs{i}.obs);
+    for j=1:length(indx)
+        if ~isempty(indx{j})
+            obsIndx = j;
+        end 
+    end
+    obsProbs(index,obsIndx) = obsProbs(index,obsIndx)/sum(obsProbs(index,:));
+end
+
+end
