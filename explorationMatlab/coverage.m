95a75a88cc981385d92dd4cdd533d351fb402200
gpaul1611 <gpaul1611@dfc047db-ddda-4613-bb57-a2f62a239487>
2007-11-20 05:17:31 +0000

    Coverage code update and attempting to develop the blasting_posesel so its better

---
+++ explorationMatlab/coverage.m
@@ -0,0 +1,173 @@
+%% streamOnto_mine_manystarts
+%
+% *Description:* This determines the path and coverage for each
+
+function coverage()
+
+close all
+
+%% Variables
+%first time you need to load the mesh
+load meshNplanes
+% plot_planes(plane,0.04);
+global r
+
+%define mew
+mew=0.04;
+
+Q=[0,0,0,0,0,0];
+
+quickver=false;
+
+findjointconfig=false;
+
+numofitts=10;
+
+%% Get all joint configs
+if findjointconfig
+    display('Getting joint configurations for all planes');
+    tic; warning off;
+    for i=1:size(plane,2)
+        if i==1; 
+            previous_q=Q; 
+        else
+            if j_sol(i-1).solutionvalid && ~isempty(find(isnan(previous_q),1)); 
+                previous_q=j_sol(i-1).qt; 
+            else previous_q=Q; 
+            end
+        end
+
+        try [j_sol(i).qt,j_sol(i).solutionvalid,j_sol(i).dist_val,j_sol(i).targetdist,j_sol(i).used_sol] = ...
+            blasting_posesel(r, plane(i).home_point, plane(i).equ, previous_q, quickver);        
+
+            if ~isempty(find(isnan(j_sol(i).qt),1))
+                j_sol(i).solutionvalid=0;
+            end
+    %         j_sol(i)
+        catch
+            j_sol(i).solutionvalid=0;
+        end
+    end
+    save('j_sol.mat','j_sol');
+    toc; warning on;
+else %load it from file
+    load j_sol.mat
+end
+
+allsols=[];for i=1:size(j_sol,2);allsols=[allsols;j_sol(i).solutionvalid];end
+allsols_equal_1=find(allsols==1);
+
+
+%% Go through number of itterations
+for currentit=1:numofitts
+    jointdiffweight=0;
+    coverageweight=0;
+    
+%% Setting up converage: The mesh points      
+    figure(1)    
+    cla('reset');
+    P0=points;
+    subplot(3,1,2)
+    plot3(P0(:,1),P0(:,2),P0(:,3),'g.');hold on;axis equal
+    P0cover=zeros(size(P0,1),1);
+    
+%% Generate a random list
+    listofplanes=1:size(allsols_equal_1,1);
+    randvals=rand(1,size(allsols_equal_1,1));
+    [notused,order]=sort(randvals);
+    rand_listofplanes=allsols_equal_1(listofplanes(order));
+
+%% Go through each of the list
+    previous_q=j_sol(rand_listofplanes(1)).qt;
+    
+    for i=rand_listofplanes(2:end)'
+        jointdiffweight=jointdiffweight+abs(j_sol(i).qt-previous_q);
+
+        %start blast point
+        P1=plane(i-1).home_point;
+
+        %End blast point
+        P2=plane(i).home_point;
+
+        %distance between P1 and P2
+        D12=sqrt((P1(1)-P2(1))^2+(P1(2)-P2(2))^2+(P1(3)-P2(3))^2);
+        
+        % Arbitary test to see if close enough between planes
+    %     if D12<4*mew
+            try delete(h1);end
+            try delete(h2);end
+            h1=plot3(P1(1),P1(2),P1(3),'r*');
+            h2=plot3(P2(1),P2(2),P2(3),'b*');
+    %     else
+            %     display('not close enough');
+    %         continue;
+    %     end
+
+
+
+        %get distance from P1 and P2 to all mesh points
+        D02=sqrt((P0(:,1)-P2(1)).^2+(P0(:,2)-P2(2)).^2+(P0(:,3)-P2(3)).^2);
+        D10=sqrt((P1(1)-P0(:,1)).^2+(P1(2)-P0(:,2)).^2+(P1(3)-P0(:,3)).^2);
+
+        %fill out mat so same size for cross product
+        P2minusP1mat=[ones(size(P0,1),1)*P2(1)-P1(1),...
+                      ones(size(P0,1),1)*P2(2)-P1(2),...
+                      ones(size(P0,1),1)*P2(3)-P1(3)];
+
+        %get perp distances to surface
+        % see this page %
+        % http://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
+        Dh=sqrt(...
+                sum(...
+                    cross(...
+                           P2minusP1mat,...
+                           [P1(1)-P0(:,1),P1(2)-P0(:,2),P1(3)-P0(:,3)]...
+                           )...
+                    .^2 ...
+                    ,2)...
+                )...
+             /...
+             sqrt(...
+                  sum(...
+                      (P2-P1).^2 ...
+                      )...
+                  );
+        %Find points on mesh covered
+        %%% mew from line AND 
+        %%% where the sum of distance from each home point to the mesh points is less than then the distance between home points + mew
+        newlycovered=find(Dh<mew &  D02+D10<D12+mew);
+        P0cover(newlycovered)=P0cover(newlycovered)+0.1;
+        %make sure it can't have a value greater than 1
+%         P0cover(find(P0cover>1))=1;
+    
+        % update the previous q
+        previous_q=j_sol(i).qt;
+    end
+    
+    %display results
+    display(['Current Itteration = ',num2str(currentit)]);
+    display(['Joint diff weight in rads= ',num2str(jointdiffweight)]);
+    figure (1)
+    subplot(3,1,1);    
+    bar(jointdiffweight)
+    title('Absolute Joint Change for each of 6 joints (rads)')
+    
+    subplot(3,1,2);
+    title('Coverage map')
+    for j=0.1:0.1:1
+        try delete(plothandle(j/0.1)); end
+        if j<1
+            covered=find(P0cover==j);
+        else %when j==1
+            covered=find(P0cover>=j);
+        end
+        if ~isempty(covered)
+            plothandle(int32(j/0.1))=plot3(P0(covered,1),P0(covered,2),P0(covered,3),'color',[1-j,1-j,1-j],'marker','.','linestyle','none');
+            drawnow
+        end
+    end
+    subplot(3,1,3);    
+    hist(P0cover,0:0.1:max(P0cover))
+    title('Coverage histogram each 0.1 is covered once');
+    uiwait(msgbox('Press to continue'));
+end
---
+++ explorationMatlab/setupoptimisation_fordevelopement.m
@@ -0,0 +1,60 @@
+%% setupoptimisation
+%
+% *Description:*  this sets up the variables for the optimisation of the robot
+% and Exploration, this can be run by itself, nothing needs to be setup
+
+%% Function Call
+% 
+% *Inputs:* NULL
+%
+% *Returns:* NULL
+
+function setupoptimisation()
+
+%% Variables
+global optimise
+
+%% Inverse kinematics:
+%this is the min stream length
+optimise.mintargetdis=0.15;
+%this is the max stream length
+optimise.maxtargetdis=0.50;
+%this is the min distance between where stream hits target the target and
+%where we desire it to hit (plane.home_point)
+optimise.minAccepDis=0.012;
+% This is how many max iterations for the optimisation
+optimise.iLimit=150;%used to be 1000
+% This is the acceptable value for the cost funciton minimisation
+optimise.stol = 1e-10;%used to be 1e-13
+% This is weighting for the jointmoveweight - which we want to minimise 
+optimise.jointmoveweight=1;
+% and the additional info which we want to maximise
+optimise.addinfoweight=50;  
+%the max angular deflection (in rads) from requested in pose selection
+optimise.maxDeflectionError = pi/5;%used to be pi/8;
+% Min joint resolution (guess) 0.0005 of a degree
+optimise.jointresolution=1e-5;
+
+%% Path plannning
+%How many times we try and find a path before stopping
+optimise.numofPPiterations=50;
+%max angle (rads) for the joints 1,2,3 so we work out incremental steps
+%with no collisions. Initially this was max_angle_for123=[9,11,20]*pi/180; 
+%As worked out with cos formul to keep max posible movement at end effector
+%to be approx 150mm and about the same size as safety dist in end ellipse 
+optimise.max_angle_for123=[4,5,7]*pi/180; %Less than 1/2 calculated for safety
+
+%% NBV
+%this is how many views to save for the NBV
+optimise.num_bestviews=60;
+%This is how many paths to get for NBV
+optimise.valid_max=20; 
+
+%% NBV_beta
+%this will be multiplied by the percentage of the points that we know in
+%space around the robot. It we know nothing then most possibliities will
+%pass through but there is less change that the robot arm will be allowed
+%at that spot. As we know more and the tr(1:3,4) of the arm in nbv_beta
+%results in safe places this dampner will stop all possibilites being
+%explored and only some will be looked at (still random)
+optimise.nbv_beta_dampner=0.8;
