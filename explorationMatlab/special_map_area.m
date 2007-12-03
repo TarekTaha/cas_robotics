c45715f88a760f6222209816f4076e86c9d1da4a
gpaul1611 <gpaul1611@dfc047db-ddda-4613-bb57-a2f62a239487>
2007-12-03 06:03:52 +0000

    integrated the special_map_area.m into the code should now include this when it gets weighted info

--- explorationMatlab/special_map_area.m
+++ explorationMatlab/special_map_area.m
@@ -1,33 +1,28 @@
 function special_map_area()
-load ScanforClassifier-0to-60
-global RangeData PoseData PointData workspace scan
-workspace.spec_pnts=[];
+% load ScanforClassifier-0to-60
+global RangeData PointData workspace
+tic
+% workspace.spec_pnts=[];
 
 [zeroRangeCol,zeroRangeRow]=find(RangeData<20);
-% NewRangeData=RangeData;
-% for i=1:size(zeroRangeCol,1)
-%     NewRangeData(zeroRangeCol(i),zeroRangeRow(i))=scan.size*1000;
-% end
 
 %diff te pan direction
 diffval=diff(RangeData,1,2);
 
-[diffCol,diffRow]=find(diffval>100);
+[diffCol,diffRow]=find(diffval>workspace.inc_size*1000);
 
-% ssect=find(a>130& a<132);
-% 
-% a(ssect(1))
-% b(ssect(1))
-% 
+% Check that it is not a spike caused by a zero value eg +150 then -150 so
+% check the sum over several values
+% This also makes sure we don't included specled (varying quickly without
+% any value about shadowing)
 Istokeep=[];
 for i=1:size(diffCol,1)
-%     check if it quickly levels again
     if size(diffval,2)>(diffRow(i)+5)
-        if abs(sum(diffval(diffCol(i),diffRow(i):diffRow(i)+5)))>50
+        if abs(sum(diffval(diffCol(i),diffRow(i):diffRow(i)+5)))>workspace.inc_size*1000
             Istokeep=[Istokeep,i];
         end
     else
-        if abs(sum(diffval(diffCol(i),diffRow(i):end)))>50
+        if abs(sum(diffval(diffCol(i),diffRow(i):end)))>workspace.inc_size*1000
             Istokeep=[Istokeep,i];
         end    
     end
@@ -36,72 +31,39 @@ end
 diffCol=diffCol(Istokeep);
 diffRow=diffRow(Istokeep);
 
-% for i=1:size(a,1);
-%     diffval(a(i),b(i)-1:b(i)+1)
-%     pause
-% end
-
-%special points are the diffCol,diffRow->diffRow+1 values
-
+%special points are the diffCol,diffRow->diffRow+1 values where neither is
+%a zero return (these are ignored in this section since it is too hard to
+%calculate the areas of interest
 for i=1:size(diffCol,1)
-    radius=sqrt(sum(((PointData(diffCol(i),diffRow(i),:)-PointData(diffCol(i),diffRow(i)+1,:))/2).^2));
-    %make sure the radius is not too large
-    if radius>2*workspace.inc_size; radius=2*workspace.inc_size; end
-    
-    %can't use the PointData as was calculated if we substituted
+    %check if either point is from a zero range point
     if isempty(find(diffCol(i)==zeroRangeCol & zeroRangeRow==diffRow(i),1)) &&...
             isempty(find(diffCol(i)==zeroRangeCol & zeroRangeRow==diffRow(i)+1,1))
+        
+        radius=sqrt(sum(((PointData(diffCol(i),diffRow(i),:)-PointData(diffCol(i),diffRow(i)+1,:))/2).^2));
+        %make sure the radius is not too large
+        if radius>2*workspace.inc_size; radius=2*workspace.inc_size; end
+
+        %Determine the mid point between the points with the big
+        %differential
         special_point=squeeze((PointData(diffCol(i),diffRow(i),:)+PointData(diffCol(i),diffRow(i)+1,:))/2)';
-    else
-        continue
-%         %special point is half between new and old 
-%         %pick the min range val point 
-%         if NewRangeData(diffCol(i),diffRow(i))<NewRangeData(diffCol(i),diffRow(i)+1)
-%            %move shorter one back to the origin by taking away scaner pos
-%            shiftedtoorigin=squeeze(PointData(diffCol(i),diffRow(i),:))'-squeeze(PoseData(diffCol(i),1:3,4));
-%         else
-%            %move shorter one back to the origin by taking away scaner pos
-%            shiftedtoorigin=squeeze(PointData(diffCol(i),diffRow(i)+1,:))'-squeeze(PoseData(diffCol(i),1:3,4));
-%         end
-%         %increase the first by the factor of its size and the radius        
-%         scaled=shiftedtoorigin*(NewRangeData(diffCol(i),diffRow(i))+radius*1000)/NewRangeData(diffCol(i),diffRow(i));
-%         %add vector to the scanner position vector
-%         special_point=scaled+squeeze(PoseData(diffCol(i),1:3,4));
+
+        %put the point surrounding the special point in areas of interest
+        %since they will mean a more complete map
+        workspace.spec_pnts=[workspace.spec_pnts;...
+            workspace.unknowncoords(...
+            find(sqrt((workspace.unknowncoords(:,1)-special_point(1)).^2+...
+                      (workspace.unknowncoords(:,2)-special_point(2)).^2+...
+                      (workspace.unknowncoords(:,3)-special_point(3)).^2)<radius),:)];   
     end
-
-    
-    display(['Range vals are ',num2str(RangeData(diffCol(i),diffRow(i))),' to ',num2str(RangeData(diffCol(i),diffRow(i)+1))]);
-    display(['Diff value is',num2str(diffval(diffCol(i),diffRow(i)))])
-    
-    
-    temp1=plot3(PointData(diffCol(i),diffRow(i),1),PointData(diffCol(i),diffRow(i),2),PointData(diffCol(i),diffRow(i),3),'r.');
-    hold on;
-    temp2=plot3(PointData(diffCol(i),diffRow(i)+1,1),PointData(diffCol(i),diffRow(i)+1,2),PointData(diffCol(i),diffRow(i)+1,3),'r.');    
-    temp3=plot3(special_point(1),special_point(2),special_point(3),'b*');
-    view(3);
-    workspace.spec_pnts=[workspace.spec_pnts;...
-        workspace.unknowncoords(...
-        find(sqrt((workspace.unknowncoords(:,1)-special_point(1)).^2+...
-                  (workspace.unknowncoords(:,2)-special_point(2)).^2+...
-                  (workspace.unknowncoords(:,3)-special_point(3)).^2)<radius),:)];
-              
-    plot3(workspace.spec_pnts(:,1),workspace.spec_pnts(:,2),workspace.spec_pnts(:,3),'k*');
-%     uiwait(msgbox('ok to continue'));
-
-drawnow
-pause(0.5);
-try delete(temp1);end
-try delete(temp2);end
-try delete(temp3);end
-      cla('reset')    
-
-    
 end
 
 %normalise to the workspace size
 workspace.spec_pnts=unique(workspace.spec_pnts,'rows');
 workspace.spec_pnts=round(workspace.spec_pnts/workspace.inc_size)*workspace.inc_size;
+keyboard
+workspace.spec_pnts=setdiff(workspace.spec_pnts,all_known=[workspace.knowncoords;workspace.indexedobsticles]);
 
-plot3(workspace.spec_pnts(:,1),workspace.spec_pnts(:,2),workspace.spec_pnts(:,3),'k*');
-hold on;
-plot3(PointData(:,:,1),PointData(:,:,2),PointData(:,:,3),'g.')
+% plot3(workspace.spec_pnts(:,1),workspace.spec_pnts(:,2),workspace.spec_pnts(:,3),'k*');
+% hold on;
+% plot3(PointData(:,:,1),PointData(:,:,2),PointData(:,:,3),'g.')
+toc
