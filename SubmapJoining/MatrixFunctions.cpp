1ebf700c20e9aca73d9fa1cc8d6ab568cfbd7807
joehe054 <joehe054@dfc047db-ddda-4613-bb57-a2f62a239487>
2008-01-08 07:02:49 +0000

    cholesky just implemented and a first try worked

--- SubmapJoining/MatrixFunctions.cpp
+++ SubmapJoining/MatrixFunctions.cpp
@@ -1,6 +1,6 @@
 #include "MatrixFunctions.h"
 
-Matrix sparse_matrix_to_dence_matrix(const SparseMatrix& md){
+Matrix to_dence_matrix(const SparseMatrix& md){
   Matrix result(md.rows, md.cols);
   SparseMatrixElement *row_ptr;
   for(int i = 1; i <= md.rows; ++i){
@@ -12,4 +12,51 @@ Matrix sparse_matrix_to_dence_matrix(const SparseMatrix& md){
   }
 
   return result;
+}
+
+Matrix to_dence_matrix(const SparseSymmMatrix& md){
+  Matrix result(md.rows, md.cols);
+  SparseMatrixElement *row_ptr;
+  for(int i = 1; i <= md.rows; ++i){
+	  row_ptr = md.first_in_row[i];
+	  while(row_ptr){
+		  result.set(row_ptr->row, row_ptr->col, row_ptr->value);
+		  result.set(row_ptr->col, row_ptr->row, row_ptr->value);
+		  row_ptr = row_ptr->next_in_row;
+	  }
+  }
+
+  return result;
+}
+
+SparseMatrix cholesky(SparseSymmMatrix m){
+	SparseMatrix result(m.rows, m.cols);
+	SparseMatrixElement *row_ptr1;
+	SparseMatrixElement *row_ptr2;
+	//SparseMatrixElement *last_added;
+	double lambda;
+	for(int i = 1; i <= m.rows; ++i){
+		m.print();
+		row_ptr1 = m.first_in_row[i];
+		if(m.first_in_row[i]){
+			lambda = sqrt(m.first_in_row[i]->value);
+			result.set(i, i, lambda);
+			row_ptr1 = row_ptr1->next_in_row;
+			while(row_ptr1){
+				result.set(i, row_ptr1->col, row_ptr1->value/lambda);
+				row_ptr1 = row_ptr1->next_in_row;
+			}
+		}
+		row_ptr1 = result.first_in_row[i];
+		while(row_ptr1){
+			row_ptr2 = result.first_in_row[i];
+			while(row_ptr2 != row_ptr1){
+				m.add(row_ptr2->col, row_ptr1->col, -row_ptr1->value * row_ptr2->value);
+				row_ptr2 = row_ptr2->next_in_row;
+			}
+			m.add(row_ptr1->col, row_ptr2->col, -row_ptr1->value * row_ptr2->value);
+			row_ptr1 = row_ptr1->next_in_row;
+		}
+	}
+	return result;
 }
--- SubmapJoining/sparse_test.cpp
+++ SubmapJoining/sparse_test.cpp
@@ -4,27 +4,83 @@
 
 using namespace std;
 
-int  main(int argc, char *argv[]){
+SparseMatrix random_matrix(int rows, int cols, int num_nonzero){
+	SparseMatrix result(rows, cols);
+	int tmp_row, tmp_col, tmp_value;
+	for(int i = 0; i < num_nonzero; ++i){
+		tmp_row = (int)(rows * (double)rand()/(double)RAND_MAX) + 1;
+		tmp_col = (int)(cols * (double)rand()/(double)RAND_MAX) + 1;
+		tmp_value = (int)(9 * (double)rand()/(double)RAND_MAX) + 1;
+		result.set(tmp_row, tmp_col, tmp_value);
+	}
+	return result;
+}
 
-  SparseMatrix sA(6,6);
-  sA.set(1,1, 1);
-  sA.set(2,2, 1);
-  sA.set(2,4, 1);
-  SparseMatrix sB(sA);
-  sA.set(2,5, 2);
-  sA.set(6,4, 2);
-  sB.set(6,6,6);
+SparseSymmMatrix random_symm_matrix(int rows, int cols, int num_nonzero){
+	SparseSymmMatrix result(rows, cols);
+	int tmp_row, tmp_col;
+	double tmp_value;
+	for(int i = 0; i < num_nonzero; ++i){
+		tmp_row = (int)(rows * (double)rand()/(double)RAND_MAX) + 1;
+		tmp_col = (int)(cols * (double)rand()/(double)RAND_MAX) + 1;
+		//cout << tmp_row << " "<< tmp_col << endl; 
+		tmp_value = (int)(9 * (double)rand()/(double)RAND_MAX) + 1;
+		result.set(tmp_row, tmp_col, tmp_value);
+	}
+	return result;
+}
+
+int main(int argc, char *argv[]){
+
+	SparseSymmMatrix sA(6,6);
+	sA.set(1,1, 1);
+	sA.set(2,2, 1);
+	sA.set(2,4, 1);
+
+	sA.set(2,5, 2);
+	sA.set(2,6, 2);
+	sA.set(2,6, 1);
+	sA.set(6,4, 2);
+	sA.add(6,4, 1);
+	SparseSymmMatrix sB(sA);
+	sB.set(6,6,6);
+
+	Matrix dA, dB, dC;
+  
+	SparseSymmMatrix sC(2,2);
+  
+	for(int i = 0; i < 0; ++i){
+		sA = random_symm_matrix(50,50, 1000);
+		sB = random_symm_matrix(50,50, 1000);
+		sC = sA + sB;
 
   
+		dA = to_dence_matrix(sA);
+		dB = to_dence_matrix(sB);
+		dC = to_dence_matrix(sC);
 
-  
-  Matrix dA = sparse_matrix_to_dence_matrix(sA);
+		if(dC == dA + dB){}
+		else{
+			cout << "dA" << endl;
+			dA.print();
+			cout << "dB" << endl;
+			dB.print();
+			cout << "dC" << endl;
+			dC.print();
+			cout << "sA" << endl;
+			sA.print();
+			cout << "sB" << endl;
+			sB.print();
+			cout << "sC" << endl;
+			sC.print();
+		}
+	}
+	sC.set(1,1,2);
+	sC.set(2,2,2);
+	sC.set(1,2,1);
+	cholesky(sC).print();
+  cout << "finish" <<endl;
 
-  
-  cout << "sA" << endl;
-  sA.print();
-  cout << "sB" << endl;
-  sB.print();
 
   return 1;
 }
