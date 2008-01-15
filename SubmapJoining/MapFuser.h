9b469a902e10655215485cff7fe4ff05f0d061ff
joehe054 <joehe054@dfc047db-ddda-4613-bb57-a2f62a239487>
2008-01-15 03:09:07 +0000

    works for localmap2

--- SubmapJoining/MapFuser.h
+++ SubmapJoining/MapFuser.h
@@ -15,10 +15,11 @@ class MapFuser{
 	public:
 		MapFuser();
 		GlobalMap glb_map;
-		void update_map();
+		void update_map(const Matrix& obsX, const Matrix& obsP);
 		void fuse_map(LocalMap m);
 		void fuse_first_map(LocalMap m);
 		void set_potential_assosiations();
+		void add_new_beacons_and_robot_location_to_state(const Matrix& obsX);
 		Matrix get_part_of_X_for_assositation();
 		Matrix restore_part_of_P_for_assositation();
 		Matrix trans_cov_matrix_to_local_cordinate_system(const Matrix& P, const Matrix& X);
@@ -30,8 +31,10 @@ class MapFuser{
 		int num_potential_assosiations;
 		int assosiations[100];
 		int assosiation_matches[30];
+		int num_matches;
 		int num_assositations;
 		int submaps_first_beacon[1000];
+		int num_beacons_in_submap[1000];
 		int place_of_beacon[10000];
 		int num_submaps;
 		int num_beacons;
