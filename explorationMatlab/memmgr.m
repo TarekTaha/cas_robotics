6ef9d8bbbacfed185dbd2645dd9e05b8eb7f16d5
gpaul1611 <gpaul1611@dfc047db-ddda-4613-bb57-a2f62a239487>
2008-05-20 11:15:18 +0000

    memmgr file updated

--- explorationMatlab/memmgr.m
+++ explorationMatlab/memmgr.m
@@ -12,21 +12,41 @@ globvarsinmem=whos('global');
 a=input('which var to delete? {-1=end} Type 1 (norm) or 2 (glob)'); 
 
 while a>0
-    b=input('now type var numb from top\n');
+    b=input('now type var numb from top or (0) to delete all, (-1) to quit\n');
+    
+    if b==-1; return; end
+    
     if a>=1 && a<2
-        todelete=varsinmem(round(b));
-        reply = input(['Are you sure you want to delete ',todelete.name,'? y/n [n]: '], 's');
-        if strcmp(reply,'y') || strcmp(reply,'Y')
-            thewords=['clear ', eval('todelete.name')];
-            eval(thewords)           
-        end
+        if b>0
+            todelete=varsinmem(round(b));
+            reply = input(['Are you sure you want to delete ',todelete.name,'? y/n [n]: '], 's');
+            if strcmp(reply,'y') || strcmp(reply,'Y')
+                thewords=['clear ', eval('todelete.name')];
+                eval(thewords)           
+            end
+        else
+            reply = input('Are you sure you want to delete ALL? y/n [n]: ', 's');
+            if strcmp(reply,'y') || strcmp(reply,'Y')
+                thewords='clear all';
+                eval(thewords)           
+            end            
+        end        
     elseif  a>=2 && a<3
-        todelete=globvarsinmem(round(b));
-        reply = input(['Are you sure you want to delete ',todelete.name,'? y/n [n]: '], 's');
-        if strcmp(reply,'y') || strcmp(reply,'Y')
-            thewords=['clear global ', eval('todelete.name')];
-            eval(thewords)
+        if b>0
+            todelete=globvarsinmem(round(b));
+            reply = input(['Are you sure you want to delete ',todelete.name,'? y/n [n]: '], 's');
+            if strcmp(reply,'y') || strcmp(reply,'Y')
+                thewords=['clear global ', eval('todelete.name')];
+                eval(thewords)
+            end
+        else
+            reply = input('Are you sure you want to delete ALL globals? y/n [n]: ', 's');
+            if strcmp(reply,'y') || strcmp(reply,'Y')
+                thewords='clear all global';
+                eval(thewords)           
+            end            
         end
+           
     end
     a=input('which var to delete? {-1=end} Type 1 (norm) or 2 (glob)'); 
 end
