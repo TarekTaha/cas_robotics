650c9fbd4371c8ca39f454a0a9293a6e3ca28e12
gpaul1611 <gpaul1611@dfc047db-ddda-4613-bb57-a2f62a239487>
2008-07-17 05:37:59 +0000

    so that it works in linux where the file is not compiled

--- explorationMatlab/Tools/putInVoxels_gp.m
+++ explorationMatlab/Tools/putInVoxels_gp.m
@@ -3,10 +3,13 @@ function b=putInVoxels_gp(a,inc_size)
 
 %run the special c function for returning unique put into voxels
 % there is a list, followed by a repeated last term, then all zeros
-b=putInVoxels(a,inc_size);
-%find the first repeat (start looking from front)
-firstrepeat=find(b(2:end,1)-b(1:end-1,1)==0 & b(2:end,2)-b(1:end-1,2)==0 & b(2:end,3)-b(1:end-1,3)==0,1);
-%return only unique set
-if ~isempty(firstrepeat)
-    b=b(1:firstrepeat,:);
-end    
+try b=putInVoxels(a,inc_size);
+    %find the first repeat (start looking from front)
+    firstrepeat=find(b(2:end,1)-b(1:end-1,1)==0 & b(2:end,2)-b(1:end-1,2)==0 & b(2:end,3)-b(1:end-1,3)==0,1);
+    %return only unique set
+    if ~isempty(firstrepeat)
+        b=b(1:firstrepeat,:);
+    end    
+catch
+    b=unique(round(a/inc_size))*inc_size;
+end
